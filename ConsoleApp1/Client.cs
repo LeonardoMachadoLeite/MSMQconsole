480753301ffb0b70f30fc16fce96cb21c2efe490
Leonardo Machado Leite <38584731+LeonardoMachadoLeite@users.noreply.github.com>
2019-11-23 17:44:43 -0300

    Initial Upload

---
+++ ConsoleApp1/Client.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{ 
+    class Client
+    {
+
+        static void Main(string[] args)
+        {
+
+            string username = "Client";
+            //Thread.Sleep(2000);
+            string server_ip = "192.168.15.1";
+            Console.WriteLine("Trying to connect to " + server_ip);
+            MessageQueue appQueue = new MessageQueue(@"FormatName:DIRECT=TCP:192.168.15.1\Private$\MSMQ_queue"); // 192.168.7.1  192.168.0.10
+            string input = "";
+
+            Action<object> receiveMsgsAction = (object obj) =>
+            {
+
+                Console.WriteLine("Thread start");
+
+                Message msg = appQueue.Peek();
+                msg.Formatter = new BinaryMessageFormatter();
+                Console.WriteLine(msg.Body.ToString());
+
+
+            };
+
+            Task receiveMsgs = Task.Factory.StartNew(receiveMsgsAction,"receiver");
+
+            Console.WriteLine("Você entrou na conversa");
+            input = Console.ReadLine();
+            while (!input.Equals("sair"))
+            {
+
+                Send(input,appQueue,username);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Conversa encerrada");
+            Console.ReadKey();
+        }
+
+
+
+        /*
+        static string Receive(MessageQueue appQueue)
+        {
+            Message[] msgs = appQueue.GetAllMessages();
+            bool first = true;
+            StringBuilder aux = new StringBuilder();
+
+            foreach (Message msg in msgs) {
+                msg.Formatter = new BinaryMessageFormatter();
+                if (!first)
+                {
+                    aux.Append("\n");
+                }
+                else
+                {
+                    first = false;
+                }
+                aux.Append(msg.Body.ToString());
+            }
+
+            return aux.ToString();
+        }
+        */
+        static string Receive(MessageQueue appQueue)
+        {
+            Message[] msgs = appQueue.GetAllMessages();
+            StringBuilder builder = new StringBuilder();
+            bool first = true;
+
+            foreach (Message msg in msgs)
+            {
+                if (!first)
+                {
+                    builder.Append("\n");
+                }
+                else
+                {
+                    first = false;
+                }
+                msg.Formatter = new BinaryMessageFormatter();
+                builder.Append(msg.Body.ToString());
+            }
+
+            return builder.ToString();
+        }
+        static void Send(string MessageToBeSent, MessageQueue appQueue, string username)
+        {
+            Message msg = new System.Messaging.Message();
+
+            msg.Formatter = new BinaryMessageFormatter();
+            msg.Body = MessageToBeSent;
+            msg.Label = username;
+
+            if (MessageToBeSent.StartsWith("HP:"))
+            {
+                msg.Priority = MessagePriority.Highest;
+            }
+            else
+            {
+                msg.Priority = MessagePriority.Normal;
+            }
+
+            appQueue.Send(msg);
+        }
+
+    }
+
+
+}
---
+++ MSMQconsole/Server.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSMQconsole
+{
+    class Server
+    {
+
+        static void Main(string[] args)
+        {
+
+            string username = "Servidor";
+            MessageQueue appQueue;
+
+            if (MessageQueue.Exists(@".\Private$\MSMQ_queue"))
+            {
+                appQueue = new MessageQueue(@".\Private$\MSMQ_queue");
+            }
+            else
+            {
+                appQueue = MessageQueue.Create(@".\Private$\MSMQ_queue");
+            }
+
+
+            //appQueue.
+            appQueue.Purge();
+            Console.WriteLine("Chat inicializado");
+            string input = "";
+
+            /*Action<object> receiveMsgsAction = (object obj) =>
+            {
+                while (input != "parar")
+                {
+                    Console.Clear();
+                    Console.WriteLine(Receive(appQueue));
+                }
+            };*/
+
+            Action<object> receiveMsgsAction = (object obj) =>
+            {
+
+                int msgs_amount = 0;
+
+                while (input != "sair")
+                {
+                    Message[] msgs = appQueue.GetAllMessages();
+
+                    if (msgs_amount != msgs.Length)
+                    {
+                        msgs_amount = msgs.Length;
+                        StringBuilder builder = new StringBuilder();
+                        bool first = true;
+
+                        foreach (Message msg in msgs)
+                        {
+                            if (!first)
+                            {
+                                builder.Append("\n");
+                            }
+                            else
+                            {
+                                first = false;
+                            }
+                            msg.Formatter = new BinaryMessageFormatter();
+                            builder.Append(msg.Body.ToString());
+                        }
+
+                        Console.Clear();
+                        Console.WriteLine("Chat inicializado");
+                        Console.WriteLine(builder.ToString());
+                    }
+
+                }
+            };
+
+            Task receiveMsgs = Task.Factory.StartNew(receiveMsgsAction, "receiver");
+            /*
+            input = Console.ReadLine();
+            while (!input.Equals("parar"))
+            {
+
+                Send(input, appQueue, username);
+
+                input = Console.ReadLine();
+            }
+            */
+            Console.ReadKey();
+        }
+
+        static string Receive(MessageQueue appQueue)
+        {
+            Message[] msgs = appQueue.GetAllMessages();
+            StringBuilder builder = new StringBuilder();
+            bool first = true;
+
+            foreach (Message msg in msgs)
+            {
+                if (!first)
+                {
+                    builder.Append("\n");
+                }
+                else
+                {
+                    first = false;
+                }
+                msg.Formatter = new BinaryMessageFormatter();
+                builder.Append(msg.Body.ToString());
+            }
+
+            return builder.ToString();
+        }
+        static void Send(string MessageToBeSent, MessageQueue appQueue, string username)
+        {
+            Message msg = new System.Messaging.Message();
+
+            msg.Formatter = new BinaryMessageFormatter();
+            msg.Body = MessageToBeSent;
+            msg.Label = username;
+
+            if (MessageToBeSent.StartsWith("HP:"))
+            {
+                msg.Priority = MessagePriority.Highest;
+            }
+            else
+            {
+                msg.Priority = MessagePriority.Normal;
+            }
+
+            appQueue.Send(msg);
+        }
+
+    }
+}
